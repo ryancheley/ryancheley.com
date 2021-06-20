97ba9a094262fc144948ce1b27aa53f31e3c8c22
Ryan Cheley <rcheley@gmail.com>
2021-06-20 10:06:39 -0700

    inital Pelican setup

---
+++ pelicanconf.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = 'Ryan Cheley'
+SITENAME = 'My Place on the Internet'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+STATIC_PATHS = ['images']
+
+# Blogroll
+LINKS = (('Simon Wilison', 'https://simonwillison.net'),
+         ('Matt Layman', 'https://www.mattlayman.com'),
+         ('PyBites', 'https://pybit.es'),)
+
+# Social widget
+SOCIAL = (('Twitter', 'https://twitter.com/ryancheley/'),
+          ('GitHub', 'https://github.com/ryancheley'),
+          ('LinkedIn', 'https://www.linkedin.com/in/ryan-cheley/'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
